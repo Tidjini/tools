a4f64ca38cfe613aa145fd3b0cf3ea9840918746
Tidjini <tidjini.messaoudi@gmail.com>
2022-11-13 07:08:34 +0100

    Set Some Comments

--- base_dir.py
+++ base_dir.py
@@ -1,23 +1,25 @@
-'''BASE_DIR using pathlib
+"""BASE_DIR using pathlib
 
 use this in your django. 
 settings/base.py
-'''
+"""
 
 from pathlib import Path
 
 # settings/base.py .. settings .. project
 BASE_DIR = Path(__file__).resolve().parent
 
-print(BASE_DIR)
-MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'static_root'
 
-STATICFILES_DIR = [BASE_DIR / 'static']
+MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = BASE_DIR / "static_root"
 
+# for django staticfiles directory
+STATICFILES_DIR = [BASE_DIR / "static"]
+
+# example of using BASE_DIR, in templates
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
     }
 ]
--- env_variables.py
+++ env_variables.py
@@ -1,23 +1,24 @@
-'''Get Environment Variables for Local Configurations
+"""Get Environment Variables for Local Configurations
 
 Like Django Secret Key, store the value in your machine, and call it with Python os module.
 
 IMPORTANT: For best practice, avoid to import Django modules in settings files.
 django made an exception just for ImproperlyConfigured  
-'''
+"""
 
 import os
 from django.core.exceptions import ImproperlyConfigured
 
 
 def get_environment_variable(var_name: str, default: str = None) -> str:
-    '''Get environment variable with os module
+    """Get environment variable with os module
 
-    set default for default value, we ImproperlyConfigured Exception of django settings
-    '''
+    set default for default value
+    """
     try:
         return os.environ.get(var_name, default) if default else os.environ[var_name]
     except ImproperlyConfigured:
         message = "Set {} the environment variable, in your local machine".format(
-            var_name)
+            var_name
+        )
         raise ImproperlyConfigured(message)
